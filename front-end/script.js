2d116a59a5a2c170c1cf65a94706b5372f8ea94d
Travis Gui <maboite41@gmail.com>
2021-01-13 16:56:08 +0100

    fichier js : création de var et appel de l'API

--- front-end/script.js
+++ front-end/script.js
@@ -1,27 +1,15 @@
-function getNameAndPriceProd() {
-  //event.preventDefault();
-  var request = new XMLHttpRequest();
-  request.onreadystatechange = function() {
-        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-            var response = JSON.parse(this.responseText);
-            var produit1 = document.getElementById('product1_title');
-            produit1.innerHTML = response.name;
-            console.log(response);
-      }
-    };
-    request.open("GET", "http://localhost:3000/api/furniture/");
-    request.send();
-    }
+/*Génération de l'URL de l'API selon le choix de produit à vendre
+******************************************************************/
 
-getNameAndPriceProd();
-
-
-/*const produitSell = "furniture"  //Au choix entre : "cameras" - "furniture" - "teddies"
+const produitSell = "furniture"  //Au choix entre : "cameras" - "furniture" - "teddies"
 const APIURL = "http://localhost:3000/api/" + produitSell + "/";
 
-//id du produit pour permettre un tri dans l'API
+//Variable pour l'id du produit pour permettre un tri dans l'API
 
 let idProduit = "";
+
+/*Appel de l'API
+**********************************************/
 
 getProduits = () =>{
 	return new Promise((resolve) =>{
@@ -46,34 +34,3 @@ getProduits = () =>{
 		request.send();
 	});
 };
-
-async function allProductsList(){
-		const produits = await getProduits();
-
-		//Pour chaque produit de l'API on créé l'encadré HTML du produit
-		produits.forEach((produit) =>
-		{ 
-            
-
-            let produitName = document.createElement("h2");
-            let produitPrice = document.createElement("p");
-            let produitLink = document.createElement("a");
-
-            produitLink.setAttribute("href", "product.html?id=" + produit._id);
-
-            let main = document.getElementById('product1');
-            main.appendChild(produitName);
-            main.appendChild(produitPrice);
-            main.appendChild(produitLink);
-
-            produitName.textContent = produit.name;
-            produitPrice.textContent = produit.price / 100 + " euros";
-            produitLink.textContent = "Voir le produit";
-
-            //Déterminer le contenu des balises
-            //produitName.innerHTML=produit.name;
-            //produitPrice.innerHTML=produit.price;
-      });
-    };
-    
-    allProductsList();*/
