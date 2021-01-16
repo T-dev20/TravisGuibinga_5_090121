3d2f7fd569cff746bda868004285770f9b5e2544
Travis Gui <maboite41@gmail.com>
2021-01-16 16:45:54 +0100

    déplacement d'un bloc de code

--- front-end/script.js
+++ front-end/script.js
@@ -8,19 +8,6 @@ const APIURL = "http://localhost:3000/api/" + productSelect + "/";
 
 let idProduct = "";
 
-/*L'utilisateur à besoin d'un panier dans le localStorage de son navigateur
-On vérifie que le panier existe dans le localStorage, sinon on le crée et on l'envoie dans le localStorage au premier chargement du site quelque soit la page*/
-if(localStorage.getItem("basketUser")){
-	console.log("Administration : Panier présent dans le localStorage");
-}else{
-	console.log("Administration : Panier non présent dans le localStorage. Créons-le!");
-	let basketInit = [];
-	localStorage.setItem("basketUser", basketInit);
-}
-
-//Récupération du panier
-let userPanier = localStorage.getItem("basketUser");
-
 /*Appel de l'API
 **********************************************/
 
@@ -100,3 +87,17 @@ provideProducts = () =>{
 			productLink.textContent = "Voir le produit";
 		});
 };
+
+/*L'utilisateur à besoin d'un panier dans le localStorage de son navigateur
+On vérifie que le panier existe dans le localStorage, sinon on le crée et on l'envoie dans le localStorage au premier chargement du site quelque soit la page*/
+
+if(localStorage.getItem("basketUser")){
+	console.log("Administration : Panier présent dans le localStorage");
+}else{
+	console.log("Administration : Panier non présent dans le localStorage. Créons-le!");
+	let basketInit = [];
+	localStorage.setItem("basketUser", basketInit);
+}
+
+//Récupération du panier
+let userPanier = localStorage.getItem("basketUser");
