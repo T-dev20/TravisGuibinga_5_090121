c7c0dbba7da86eda1b560468c5bc91e394ca6ce5
Travis Gui <maboite41@gmail.com>
2021-01-25 17:04:56 +0100

    suppression de lignes inutiles

--- front-end/script.js
+++ front-end/script.js
@@ -271,7 +271,6 @@ recapProducts = () =>{
 };
 
 removeProduct = (i) => {
-	console.log('i', i);
 		//recupérer le array
 		userBasket.splice(i, 1); 
 		//On vide le localstorage
@@ -290,7 +289,6 @@ removeProduct = (i) => {
   //vérifie les inputs du formulaire
   checkInput = () =>{
     //Controle Regex
-    let checkString = /[a-zA-Z]/;
     let checkNumber = /[0-9]/;
     //Source pour vérification email => emailregex.com
     let checkMail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/y;
