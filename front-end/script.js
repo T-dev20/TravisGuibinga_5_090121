5c0299c2e457fe1a8e6ef5a3fa5bd6860d42653b
Travis Gui <maboite41@gmail.com>
2021-01-28 15:17:27 +0100

    fonction pour la requête POST modifiée

--- front-end/script.js
+++ front-end/script.js
@@ -350,7 +350,7 @@ let url = "http://localhost:3000/api/furniture/order";
 
 
 //Fonction requête post de l'API
-const sendData = (sendForm, url) => {
+sendData = () => {
   	return new Promise((resolve)=>{
   		let request = new XMLHttpRequest();
   		request.onload = function() {
@@ -359,14 +359,12 @@ const sendData = (sendForm, url) => {
 				sessionStorage.setItem("order", this.responseText);
 				window.location = "./confirm.html";
 				resolve(JSON.parse(this.responseText));
-				console.log(sendForm);
       }else {
       }
   };
   request.open("POST", url);
   request.setRequestHeader("Content-Type", "application/json");
-  request.send(sendForm);
-  console.log(sendForm);
+  request.send();
 });
 };
 
